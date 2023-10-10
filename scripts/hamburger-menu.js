f691a1839404d3ae894427dca8c1e21b0998fafe
lula314 <104175188+lula314@users.noreply.github.com>
2023-10-10 15:55:51 -0600

    hamburger menu works now

--- scripts/hamburger-menu.js
+++ scripts/hamburger-menu.js
@@ -1,2 +1,17 @@
-const hidebutton = document.querySelector(".menu-hide")
-const 
+const hidebutton = document.querySelector(".menu-hide");
+const showbutton = document.querySelector(".menu-show");
+const navmenu = document.querySelector("#nav-menu"); 
+
+hidebutton.addEventListener('click',() => {
+    hidebutton.classList.toggle('showing');
+    showbutton.classList.toggle('showing');
+    navmenu.classList.toggle('showing');
+    console.log("toggle");
+});
+
+showbutton.addEventListener('click',() => {
+    hidebutton.classList.toggle('showing');
+    showbutton.classList.toggle('showing');
+    navmenu.classList.toggle('showing');  
+    console.log("toggle");
+});
